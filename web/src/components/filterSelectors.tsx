3d86d68e7392fae6b2d743066a15552a825ec76a
Bülent Özden <bulentozden2007@gmail.com>
2022-09-23 20:56:47 +0300

    Change in filters

--- web/src/components/filterSelectors.tsx
+++ web/src/components/filterSelectors.tsx
@@ -8,54 +8,58 @@ import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select, { SelectChangeEvent } from "@mui/material/Select";
 import Checkbox from "@mui/material/Checkbox";
-
+// import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank';
+// App
 import { useStore } from "../stores/store";
 
-const ITEM_HEIGHT = 48;
-const ITEM_PADDING_TOP = 8;
-const MenuProps = {
-  PaperProps: {
-    style: {
-      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-      width: 250,
-    },
-  },
-};
+// const ITEM_HEIGHT = 48;
+// const ITEM_PADDING_TOP = 8;
+// const MenuProps = {
+//   PaperProps: {
+//     style: {
+//       maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+//       width: 250,
+//     },
+//   },
+// };
 
 export const FilterSelectors = () => {
   const { metaData } = useStore();
-  const [version, setVersion] = React.useState<string[]>([]);
-  const [locale, setLocale] = React.useState<string[]>([]);
+  // const [version, setVersion] = React.useState<string[]>([]);
+  // const [locale, setLocale] = React.useState<string[]>([]);
+  const {versionFilter, setVersionFilter} = useStore();
+  const {languageFilter, setLanguageFilter} = useStore();
 
-  const handleVersionFilterChange = (e: SelectChangeEvent<typeof version>) => {
+  const handleVersionFilterChange = (e: SelectChangeEvent<typeof versionFilter>) => {
     const {
       target: { value },
     } = e;
     // On autofill we get a stringified value.
-    setVersion(typeof value === "string" ? value.split(",") : value);
+    setVersionFilter(typeof value === "string" ? value.split(",") : value);
   };
 
-  const handleLocaleFilterChange = (e: SelectChangeEvent<typeof locale>) => {
+  const handleLocaleFilterChange = (e: SelectChangeEvent<typeof languageFilter>) => {
     const {
       target: { value },
     } = e;
     // On autofill we get a stringified value.
-    setLocale(typeof value === "string" ? value.split(",") : value);
+    setLanguageFilter(typeof value === "string" ? value.split(",") : value);
   };
 
   let versionList: string[] = [];
-  let localeList: string[] = [];
+  let languageList: string[] = [];
 
   if (metaData) {
     // get unique lists
     let versionCol = metaData.map((row) => {
       return row.version.toString();
     });
-    versionList = [...new Set(versionCol)];
+    versionList = [...new Set(versionCol)].sort((a,b) => Number(b) - Number(a));
+    // versionList = versionList.reverse()
     let localeCol = metaData.map((row) => {
       return row.locale;
     });
-    localeList = [...new Set(localeCol)].sort();
+    languageList = [...new Set(localeCol)].sort();
   }
 
   return !metaData ? (
@@ -72,17 +76,17 @@ export const FilterSelectors = () => {
             id="ui-version-filter-select"
             title={intl.get("ui.filter_version.title")}
             multiple
-            value={version}
+            value={versionFilter}
             onChange={handleVersionFilterChange}
             input={<OutlinedInput label="Tag" />}
             renderValue={(selected) => selected.join(", ")}
-            MenuProps={MenuProps}
+            // MenuProps={MenuProps}
             label={intl.get("ui.filter_version.label")}
           >
             {versionList.map((x) => {
               return (
                 <MenuItem key={x} value={x}>
-                  <Checkbox checked={versionList.indexOf(x) > -1} />
+                  <Checkbox checked={versionFilter.indexOf(x) > -1} />
                   <ListItemText primary={x} />
                 </MenuItem>
               );
@@ -100,17 +104,17 @@ export const FilterSelectors = () => {
             id="ui-language-filter-select"
             title={intl.get("ui.filter_language.title")}
             multiple
-            value={locale}
+            value={languageFilter}
             onChange={handleLocaleFilterChange}
             input={<OutlinedInput label="Tag" />}
             renderValue={(selected) => selected.join(", ")}
-            MenuProps={MenuProps}
+            // MenuProps={MenuProps}
             label={intl.get("ui.filter_language.label")}
           >
-            {localeList.map((x) => {
+            {languageList.map((x) => {
               return (
                 <MenuItem key={x} value={x}>
-                  <Checkbox checked={localeList.indexOf(x) > -1} />
+                  <Checkbox checked={languageFilter.indexOf(x) > -1} />
                   <ListItemText primary={x} />
                 </MenuItem>
               );
