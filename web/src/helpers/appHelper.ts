07ee8a88eb74d212c518b4271ab6a8d48075476a
Bülent Özden <bulentozden2007@gmail.com>
2022-10-27 09:59:56 +0300

    Add cleanFn

--- web/src/helpers/appHelper.ts
+++ web/src/helpers/appHelper.ts
@@ -9,3 +9,39 @@ export const appGithubURL =
   "https://github.com/HarikalarKutusu/cv-tbox-metadata-viewer";
 export const appLicenseURL =
   "https://github.com/HarikalarKutusu/cv-tbox-metadata-viewer/blob/main/LICENSE";
+
+export const cleanFn = (s: string): string => {
+  const toReplace = [
+    "?",
+    "[",
+    "]",
+    "/",
+    "\\",
+    "=",
+    "<",
+    ">",
+    ":",
+    ";",
+    ",",
+    "'",
+    '"',
+    "&",
+    "$",
+    "#",
+    "*",
+    "(",
+    ")",
+    "|",
+    "~",
+    "`",
+    "!",
+    "{",
+    "}",
+    "%",
+    "+",
+    String.fromCharCode(0),
+  ];
+  let fn = s.toLowerCase();
+  toReplace.forEach((ch) => (fn = fn.replaceAll(ch, "_")));
+  return fn;
+};
